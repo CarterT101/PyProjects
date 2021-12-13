754d8683e16bc2a28e923a19fa97956a574bb6d8
CarterT101 <92358332+CarterT101@users.noreply.github.com>
2021-12-13 09:25:06 -0800

    Django assignment

--- DjangoUniversity/Classes/classApp/models.py
+++ DjangoUniversity/Classes/classApp/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
-class djangoClasses(models.Model):
-    title = models.CharField(max_length=50)
-    courseNumber = models.DecimalField(default=0, max_digits=10000)
-    instructorName = models.CharField(max_length=50,default="",null=False)
-    duration = models.FloatField()
+class djangoClasses(models.Model):              #creating a class
+    title = models.CharField(max_length=50)     #giving attributes, different fields
+    courseNumber = models.DecimalField(default=0, max_digits=10000, decimal_places=0)
+    instructorName = models.CharField(max_length=50,default="", null=False)
+    duration = models.FloatField(max_length=50, default=0)
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title #returns name of title to the admin page to display
+
+
+
